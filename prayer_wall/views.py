53b5e805ebf3ff78538f69abd5870ba437ca1244
Bide Yan <b6yan@uwaterloo.ca>
2015-06-21 01:18:22 -0400

    add post, get prayer request api, able to post and see prayer requests

--- prayer_wall/views.py
+++ prayer_wall/views.py
@@ -2,35 +2,24 @@ from django.shortcuts import render_to_response
 from django.shortcuts import render
 from models import PrayerRequest
 from django.template import RequestContext, Context, Template
-
-
+from django.core import serializers
+from django.http import JsonResponse,HttpResponse
+import json
 
 def home(request):
+    return render(request, 'prayer_wall.html')
+
+def prayer_requests(request):
     if request.method == 'GET':
-        prayer_requests = []
-        for a in PrayerRequest.objects.all() :
-            prayer_requests.append(a.request)
-        context = {'prayer_requests':prayer_requests}
-        # fetch from data from db, give it to frontend
-        # return Template('../templates/prayer_wall.html').render(context)
-        # return render_to_response('../templates/prayer_wall.html', RequestContext(request))
-        return render(request, 'prayer_wall.html', context)
+        all_prayer_request = serializers.serialize("json", PrayerRequest.objects.all())
+        return HttpResponse(all_prayer_request)
     elif request.method == 'POST':
-        prayerRequest = unicode(request.POST['prayer-request'])
-        name = ''
-        email = ''
-        if 'prayer-name' in request.POST:
-            name = unicode(request.POST['prayer-name'])
-        if 'prayer-email' in request.POST:
-            email = unicode(request.POST['prayer-email'])
+        print "test1"
+        requestInfo = json.loads(request.body)
+        prayerRequest = requestInfo.get("prayer_request")
+        name = requestInfo.get("name")
+        email = requestInfo.get("email")
         req = PrayerRequest(name=name, email=email, request=prayerRequest, count=0)
         req.save()
-        prayer_requests = []
-        for a in PrayerRequest.objects.all() :
-            prayer_requests.append(a.request)
-
-        #print "-----------------------"
-        #print PrayerRequest.objects.all()
-        context = {'prayer_requests':prayer_requests}
-        return render(request, 'prayer_wall.html', context)
-        # return Template('../templates/prayer_wall.html').render(context)
+        all_prayer_request = serializers.serialize("json", PrayerRequest.objects.all())
+        return HttpResponse(all_prayer_request)
